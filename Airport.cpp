1ee20bf9574589fc76db6a2f68b1616ecda193d1
xz90 <xz90@illinois.edu>
2021-04-18 12:39:47 +0800

    xz90: created airport class

---
+++ Airport.cpp
@@ -0,0 +1,63 @@
+# pragma once
+# include "Airport.h"
+
+Airport::Airport() { }
+
+Airport::~Airport() {
+    airport_ID = 0;
+    airport_name = "";
+    airport_IATAcode = "";
+    airport_position = std::make_pair(0,0);
+    airport_location = std::make_pair("", "");
+}
+
+Airport::Airport(const Airport& other) {
+    airport_ID = other.getAirportID();
+    airport_name = other.getAirportName();
+    airport_IATAcode = other.getAirportIATACode();
+    airport_position = other.getAirportPosition();
+    airport_location = other.getAirportLocation();
+}
+
+Airport::Airport(unsigned ID, string name, string IATAcode, double latitude, double longitude, string city = "", string country = "", double altitude = 0) {
+    airport_ID = ID;
+    airport_name = name;
+    airport_IATAcode = IATAcode;
+    airport_position = std::make_pair(latitude, longitude);
+    airport_location = std::make_pair(city, country);
+    airport_altitude = altitude;
+}
+
+// getters for airports
+unsigned Airport::getAirportID() const {
+    return airport_ID;
+}
+
+string Airport::getAirportName() const {
+    return airport_name;
+}
+    
+string Airport::getAirportIATACode() const {
+    return airport_IATAcode;
+}
+    
+pair<double, double> Airport::getAirportPosition() const {
+    return airport_position;
+}
+    
+pair<std::string, std::string> Airport::getAirportLocation() const {
+    return airport_location;
+}
+
+double Airport::getAirportAltitude() const {
+    return airport_altitude;
+}
+
+bool Airport::operator==(const Airport& other) const {
+    if (airport_ID != other.airport_ID || airport_name != other.airport_name || airport_position != other.airport_position
+        || airport_location != other.airport_location || airport_altitude != other.airport_altitude) {
+        return false;
+    }
+    return true;
+}
+
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,32 @@
 
 #include "../catch/catch.hpp"
 #include "../readFromFile.hpp"
+#include "../Airport.h"
+
+
+// TEST CASES FOR AIRPORT CLASS
+TEST_CASE("Constructor") {
+	unsigned id = 0;
+	Airport sample(id, "test", "TEST", 1.1, 2.2 ,"Champagin", "USA", 0);
+	REQUIRE(sample.getAirportID() == 0);
+	REQUIRE(sample.getAirportName() == "test");
+	REQUIRE(sample.getAirportIATACode() == "TEST");
+	pair<double, double> sample_position = std::make_pair(1.1, 2.2);
+	REQUIRE(sample.getAirportPosition() == sample_position);
+	pair<std::string, std::string> sample_location = std::make_pair("Champaign", "USA");
+	REQUIRE(sample.getAirportLocation() == sample_location);
+}
+
+TEST_CASE("operator== for Airport class") {
+	unsigned id = 0;
+	Airport sample(id, "test", "TEST", 1.1, 2.2 ,"Champagin", "USA", 0);
+	Airport sample1(id, "test", "TEST", 1.1, 2.2 ,"Champagin", "USA", 0);
+	Airport sample2(id, "test", "TEST", 1.1, 2.3 ,"Champagin", "USA", 0);
+	REQUIRE(sample == sample1);
+	REQUIRE(!(sample == sample2));
+}
+// END TEST CASES FOR AIRPORT CLASS
+
 
 
 TEST_CASE("Verify that file_to_string works on a small example") {
